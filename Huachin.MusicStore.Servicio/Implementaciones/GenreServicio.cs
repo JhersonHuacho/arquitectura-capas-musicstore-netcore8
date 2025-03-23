860f33b3d5077f62a631b3901225b3e002f7ed4c
Francisco <jhersonhuacho@gmail.com>
2025-03-23 09:04:07 -0500

    Agregando implementación del servicio Genre

--- Huachin.MusicStore.Servicio/Implementaciones/GenreServicio.cs
+++ Huachin.MusicStore.Servicio/Implementaciones/GenreServicio.cs
@@ -3,16 +3,19 @@ using Huachin.MusicStore.Dto.Request;
 using Huachin.MusicStore.Dto.Response;
 using Huachin.MusicStore.Repositorios.Interfaces;
 using Huachin.MusicStore.Servicio.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace Huachin.MusicStore.Servicio.Implementaciones
 {
 	public class GenreServicio : IGenreServicio
 	{
 		private readonly IGenreRepositorio _genreRepositorio;
+		private readonly ILogger<GenreServicio> _logger;
 
-		public GenreServicio(IGenreRepositorio genreRepositorio)
+		public GenreServicio(IGenreRepositorio genreRepositorio, ILogger<GenreServicio> logger)
 		{
 			_genreRepositorio = genreRepositorio;
+			_logger = logger;
 		}
 
 		public async Task<BaseResponse> Registrar(GenreRequestDto request)
@@ -34,7 +37,116 @@ namespace Huachin.MusicStore.Servicio.Implementaciones
 			{
 				response.Message = "Ocurrió un error al registrar el género";
 				response.Success = false;
-				throw;
+				_logger.LogError(ex, $"{response.Message} {ex.Message}");				
+			}
+
+			return response;
+		}
+
+		public async Task<BaseResponse> UpdateAsync(int id, GenreRequestDto request)
+		{
+			var response = new BaseResponse();
+
+			try
+			{
+				var genre = await _genreRepositorio.FindAsync(id);
+				if (genre is null)
+				{
+					response.Message = "Género no encontrado";
+					response.Success = false;
+					return response;
+				}
+				genre.Name = request.Name;
+				await _genreRepositorio.UpdateAsync();
+				response.Message = "Género actualizado correctamente";
+				response.Success = true;
+			}
+			catch (Exception ex)
+			{
+				response.Message = "Ocurrió un error al actualizar el género";
+				response.Success = false;
+				_logger.LogError(ex, $"{response.Message} {ex.Message}");
+			}
+
+			return response;
+		}
+
+		public async Task<BaseResponse> DeleteAsync(int id)
+		{
+			var response = new BaseResponse();
+			try
+			{
+				var genre = await _genreRepositorio.FindAsync(id);
+				if (genre is null)
+				{
+					response.Message = "Género no encontrado";
+					response.Success = false;
+					return response;
+				}
+
+				await _genreRepositorio.DeleteAsync(id);
+				response.Message = "Género eliminado correctamente";
+				response.Success = true;
+			}
+			catch (Exception ex)
+			{
+				response.Message = "Ocurrió un error al eliminar el género";
+				response.Success = false;
+				_logger.LogError(ex, $"{response.Message} {ex.Message}");
+			}
+
+			return response;
+		}
+
+		public async Task<BaseResponseGeneric<IEnumerable<GenreResponseDto>>> GetAsyn()
+		{
+			var response = new BaseResponseGeneric<IEnumerable<GenreResponseDto>>();
+			try
+			{
+				var genres = await _genreRepositorio.ListAsync();
+				response.Data = genres.Select(x => new GenreResponseDto
+				{
+					Id = x.Id,
+					Name = x.Name
+				});
+				response.Message = "Géneros obtenidos correctamente";
+				response.Success = true;
+			}
+			catch (Exception ex)
+			{
+				response.Message = "Ocurrió un error al obtener los géneros";
+				response.Success = false;
+				_logger.LogError(ex, $"{response.Message} {ex.Message}");
+			}
+			return response;
+		}
+
+		public async Task<BaseResponseGeneric<GenreResponseDto>> GetAsyn(int id)
+		{
+			var response = new BaseResponseGeneric<GenreResponseDto>();
+			try
+			{
+				var genre = await _genreRepositorio.FindAsync(id);
+				if (genre is null)
+				{
+					response.Message = "Género no encontrado";
+					response.Success = false;
+					return response;
+				}
+
+				response.Data = new GenreResponseDto
+				{
+					Id = genre.Id,
+					Name = genre.Name
+				});
+				response.Message = "Géneros obtenidos correctamente";
+				response.Success = true;
+			}
+			catch (Exception ex)
+			{
+				response.Message = "Ocurrió un error al obtener el genero";
+				response.Success = false;
+				_logger.LogError(ex, $"{response.Message} {ex.Message}");
 			}
 
 			return response;
